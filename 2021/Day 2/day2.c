69faa92a57f045560a76ae627eb2e75c7502564a
Nathan Petrangelo <nathan.petrangelo@amd.com>
2021-12-03 16:38:50 -0500

    Completed day 2 part 2

--- 2021/Day 2/day2.c
+++ 2021/Day 2/day2.c
@@ -5,16 +5,18 @@
 typedef struct {
 	int x;
 	int depth;
+	int aim;
 } SUBMARINE;
 
 SUBMARINE* make_submarine() {
 	SUBMARINE* sub = malloc(sizeof(SUBMARINE));
 	sub->x = 0;
 	sub->depth = 0;
+	sub->aim = 0;
 	return sub;
 }
 
-int main(int argc, char **argv) {
+void part1() {
 	char str[10];
 	int mag;
 	SUBMARINE *sub = make_submarine();
@@ -36,5 +38,35 @@ int main(int argc, char **argv) {
 	}
 	printf("Product of final x and depth: %d\n", sub->x * sub->depth);
 	free(sub);
+}
+
+void part2() {
+	char str[10];
+	int mag;
+	SUBMARINE *sub = make_submarine();
+	FILE *input;
+	input = fopen("input.txt", "r");
+	printf("Opened file\n");
+	while (fscanf(input, "%s %d\n", str, &mag) != EOF) {
+		switch (str[0]) {
+			case 'f':
+				sub->x += mag;
+				sub->depth += mag * sub->aim;
+				break;
+			case 'u':
+				sub->aim -= mag;
+				break;
+			case 'd':
+				sub->aim += mag;
+				break;
+		}
+	}
+	printf("Product of final x and depth: %d\n", sub->x * sub->depth);
+	free(sub);
+}
+
+int main(int argc, char **argv) {
+// 	part1();
+	part2();
 	return 0;
 }
