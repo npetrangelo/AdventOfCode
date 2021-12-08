485e1e9fdea5b5f34626e4acddf8717eec45d728
Nathan Petrangelo <nathan.petrangelo@amd.com>
2021-12-07 22:47:11 -0500

    Finished day 7 in python

---
+++ 2021/Day 7/day7.py
@@ -0,0 +1,23 @@
+import math
+
+filename = "input.txt"
+
+def calc_fuel(crabs, pos):
+    return sum([abs(crab-pos) for crab in crabs])
+    
+def calc_fuel_gauss(crabs, pos):
+    return sum([abs(crab-pos)*(abs(crab-pos)+1)/2 for crab in crabs])
+
+def min_fuel(crabs):
+    prev = 0xFFFFFFFF
+    for pos in range(min(crabs), max(crabs)+1):
+        fuel = calc_fuel_gauss(crabs, pos)
+        if fuel > prev:
+            return prev, pos-1
+        prev = fuel
+
+with open(filename) as f:
+    crabs = [int(pos) for pos in f.readline().strip().split(",")]
+#     print(crabs)
+    fuel, min_pos = min_fuel(crabs)
+    print(f"Position {min_pos} with {fuel} fuel burned")
