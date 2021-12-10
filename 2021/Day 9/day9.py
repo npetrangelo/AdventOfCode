a5a41a30a745cc900fce71f7f0990301df51cfee
Nathan Petrangelo <nathan.petrangelo@amd.com>
2021-12-09 22:04:52 -0500

    Day 9 part 2

--- 2021/Day 9/day9.py
+++ 2021/Day 9/day9.py
@@ -1,3 +1,5 @@
+import math
+
 filename = "input.txt"
 
 heightmap = []
@@ -22,3 +24,24 @@ for y in range(len(heightmap)):
             risk += this+1
 
 print(risk)
+
+basins = []
+
+for y in range(len(heightmap)):
+    for x in range(len(heightmap[0])):
+        if heightmap[y][x] == 9:
+            continue
+        left_basin = list(filter(lambda basin: (x-1,y) in basin, basins))
+        above_basin = list(filter(lambda basin: (x,y-1) in basin, basins))
+        left_basin = left_basin[0] if len(left_basin) == 1 else set()
+        above_basin = above_basin[0] if len(above_basin) == 1 else set()
+        basin = {(x,y)} | left_basin | above_basin
+        if left_basin in basins:
+            basins.remove(left_basin)
+        if above_basin in basins:
+            basins.remove(above_basin)
+        basins.append(basin)
+
+sizes = sorted([len(basin) for basin in basins])
+
+print(math.prod(sizes[-3:]))
