2953623c2abb3df1fbbf544ebeda4af0916ea460
Nathan Petrangelo <nathan.petrangelo@amd.com>
2021-12-15 01:09:54 -0500

    Day 12 part 2

--- 2021/Day 12/day12.py
+++ 2021/Day 12/day12.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 filename = "input.txt"
 
 caves = {}
@@ -26,11 +28,40 @@ def travel1(cave, path):
 #         print(path)
         return
     for neighbor in caves[cave]:
-        if neighbor.islower() and neighbor in path:
-            continue
         if neighbor == "start":
+            continue
+        if neighbor.islower() and neighbor in path:
             continue
         travel1(neighbor, path + [neighbor])
 
 travel1("start", ["start"])
+print(num_paths)
+
+def small_cave_visted_twice(path):
+    special = {"start", "end"}
+    small_caves = list(filter(lambda c: c.islower() and c not in special, path))
+    if not small_caves:
+        return False
+    c = Counter(small_caves)
+#     print(c)
+    cave, frequency = c.most_common(1)[0]
+    return frequency > 1
+
+num_paths = 0
+
+def travel2(cave, path):
+    global num_paths
+    if cave == "end":
+        num_paths += 1
+#         print(path)
+        return
+    for neighbor in caves[cave]:
+        if neighbor == "start":
+            continue
+        if neighbor.islower() and neighbor in path and small_cave_visted_twice(path):
+#             print(path + [neighbor])
+            continue
+        travel2(neighbor, path + [neighbor])
+
+travel2("start", ["start"])
 print(num_paths)
